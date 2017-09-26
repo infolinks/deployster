8ac01355ea9a397cc67e11225077f7e2a8df1848
Arik Kfir <arik@infolinks.com>
2017-09-26 09:22:48 +0300

    allow setting GCP_ORG_ID environment variable instead of "--org-id" allow setting GCP_BILLING_ACCOUNT_ID environment variable instead of "--billing-account-id" allow setting GCP_GCR_PROJECT_ID environment variable instead of "--gcr-project" allow setting GCP_PROJECT_ID environment variable instead of "--project" allow setting ENV_NAME environment variable instead of "--env"

--- bin/apply.py
+++ bin/apply.py
@@ -4,6 +4,8 @@ import argparse
 import json
 import sys
 
+import os
+
 from gdm import execute_gdm_configurations
 from k8s import apply_kubernetes_state
 from util.environment import load_environment
@@ -18,31 +20,67 @@ def main():
     argparser.add_argument('--org-id',
                            metavar='ID',
                            type=int,
-                           required=True,
-                           help='numerical ID of the Google organization the project belongs to')
+                           default=os.environ['GCP_ORG_ID'] if 'GCP_ORG_ID' in os.environ else None,
+                           help='numerical ID of the Google organization the project belongs to; can be set through '
+                                'the GCP_ORG_ID environment variable.')
     argparser.add_argument('--billing-account-id',
                            metavar='ID',
-                           required=True,
-                           help='numerical ID of the Google billing account to assign the project to')
+                           default=os.environ[
+                               'GCP_BILLING_ACCOUNT_ID'] if 'GCP_BILLING_ACCOUNT_ID' in os.environ else None,
+                           help='numerical ID of the Google billing account to assign the project to; can be set '
+                                'the GCP_BILLING_ACCOUNT_ID environment variable.')
     argparser.add_argument('--gcr-project',
                            dest='gcr_project_id',
                            metavar='PROJECT',
-                           required=True,
-                           help='name of the GCP project to hold/provide the Google Container Registry')
+                           default=os.environ['GCP_GCR_PROJECT_ID'] if 'GCP_GCR_PROJECT_ID' in os.environ else None,
+                           help='name of the GCP project to hold/provide the Google Container Registry; can be set '
+                                'through the GCP_GCR_PROJECT_ID environment variable.')
     argparser.add_argument('--project',
                            dest='project_id',
                            metavar='PROJECT',
-                           required=True,
-                           help='name of the GCP project to hold the deployment environment, eg. "acme-my-feature"')
+                           default=os.environ['GCP_PROJECT_ID'] if 'GCP_PROJECT_ID' in os.environ else None,
+                           help='name of the GCP project to hold the deployment environment, eg. "acme-my-feature"; '
+                                'can be set through the GCP_PROJECT_ID environment variable.')
     argparser.add_argument('--env',
                            metavar='NAME',
-                           required=True,
-                           help='logical name of the environment being deployed to, eg. "my-feature"')
+                           default=os.environ['ENV_NAME'] if 'ENV_NAME' in os.environ else None,
+                           help='logical name of the environment being deployed to, eg. "my-feature"; can be set '
+                                'through the ENV_NAME environment variable.')
     argparser.add_argument('files',
                            metavar='FILE',
                            nargs='+',
                            help='list of JSON files to build the environment configuration from')
     args = argparser.parse_args()
+
+    # validate required arguments; while we could just add "required=True" to the "add_argument(..)" calls above, that
+    # would force the user to specify them on the command line EVEN WHEN the user specifies them through environemtn
+    # variables; there's unfortunately no "required unless specified as env-var" flag for "add_argument(..)" :)
+    if not args.org_id:
+        sys.stderr.write("please specify '--org-id' or set the GCP_ORG_ID environment variable\n")
+        sys.stderr.flush()
+        argparser.print_usage()
+        exit(1)
+    elif not args.billing_account_id:
+        sys.stderr.write(
+            "please specify '--billing-account-id' or set the GCP_BILLING_ACCOUNT_ID environment variable\n")
+        sys.stderr.flush()
+        argparser.print_usage()
+        exit(1)
+    elif not args.gcr_project_id:
+        sys.stderr.write("please specify '--gcr-project' or set the GCP_GCR_PROJECT_ID environment variable\n")
+        sys.stderr.flush()
+        argparser.print_usage()
+        exit(1)
+    elif not args.project_id:
+        sys.stderr.write("please specify '--project' or set the GCP_PROJECT_ID environment variable\n")
+        sys.stderr.flush()
+        argparser.print_usage()
+        exit(1)
+    elif not args.env:
+        sys.stderr.write("please specify '--env' or set the ENV_NAME environment variable\n")
+        sys.stderr.flush()
+        argparser.print_usage()
+        exit(1)
 
     # build the environment from the list of JSON files provided
     env = load_environment(args.env, args.files)
