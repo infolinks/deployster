8c285fc98c0f412425e646450e7186649cef7af1
Arik Kfir <arik@infolinks.com>
2017-09-26 09:21:07 +0300

    make GCP_SA_JSON environment variable optional print the account being used

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,28 +1,27 @@
 #!/usr/bin/env bash
 
-# obtain GCP service account JSON contents from environment variable
-if [[ -z "${GCP_SA_JSON}" ]]; then
-    echo "please provide the 'GCP_SA_JSON' environment variable" >&2
-    echo "example: docker run -e GCP_SA_JSON=\$(cat my-sa-key.json) infolinks/deployster <arguments...>" >&2
-    exit 1
-fi
+# if GCP_SA_JSON is provided, use it to activate the service account it references
+if [[ -n "${GCP_SA_JSON}" ]]; then
 
-# store it in a temporary file
-GCP_SA_JSON_FILE="/tmp/gcp_service_account.json"
-echo -nE "${GCP_SA_JSON}" > ${GCP_SA_JSON_FILE}
-if [[ $? != 0 ]]; then
-    echo "failed writing service account JSON file to '${GCP_SA_JSON_FILE}'"
-    exit 1
-fi
+    # store it in a temporary file
+    GCP_SA_JSON_FILE="/tmp/gcp_service_account.json"
+    GOOGLE_APPLICATION_CREDENTIALS="${GCP_SA_JSON_FILE}"
+    echo -nE "${GCP_SA_JSON}" > ${GCP_SA_JSON_FILE}
+    if [[ $? != 0 ]]; then
+        echo "failed writing service account JSON file to '${GCP_SA_JSON_FILE}'"
+        exit 1
+    fi
 
-# activate the service account
-gcloud auth activate-service-account --key-file=${GCP_SA_JSON_FILE}
-if [[ $? != 0 ]]; then
-    echo "failed activating service account from JSON file at '${GCP_SA_JSON_FILE}'" >&2
-    echo "JSON file contents:" >&2
-    cat ${GCP_SA_JSON_FILE} >&2
-    exit 1
+    # activate the service account
+    gcloud auth activate-service-account --key-file=${GCP_SA_JSON_FILE}
+    if [[ $? != 0 ]]; then
+        echo "failed activating service account from JSON file at '${GCP_SA_JSON_FILE}'" >&2
+        echo "JSON file contents:" >&2
+        cat ${GCP_SA_JSON_FILE} >&2
+        exit 1
+    fi
 fi
+echo "Google Cloud SDK will use GCP account: $(gcloud info --format=json | jq -r '.config.account')" >&2
 
 # proceed to actual deployment
-GOOGLE_APPLICATION_CREDENTIALS="${GCP_SA_JSON_FILE}" PYTHONUNBUFFERED=1 exec $(dirname $0)/apply.py $@
+GOOGLE_APPLICATION_CREDENTIALS="${GOOGLE_APPLICATION_CREDENTIALS}" PYTHONUNBUFFERED=1 exec $(dirname $0)/apply.py $@
