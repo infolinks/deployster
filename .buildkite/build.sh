7982003a226a688056a7b48750c82b818bad4902
Arik Kfir <arik@infolinks.com>
2017-11-09 10:12:55 +0200

    Finalize build & release workflow

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
-set -e
+TAG="${1}"
+[[ -z "${TAG}" ]] && echo "usage: $0 <tag>" && exit 1
 
-TAG="${BUILDKITE_COMMIT:=local}"
+set -ex
 
 sed -i "s/^FROM .*/FROM gcr.io\/infolinks-gcr\/deployster-gcp-base:${TAG}/g" ./resources/gcp/compute/address/Dockerfile
-sed -i "s/^FROM .*/FROM gcr.io\/infolinks-gcr\/deployster-gcp-base:${TAG}/g" ./resources/gcp/project/Dockerfile
 sed -i "s/^FROM .*/FROM gcr.io\/infolinks-gcr\/deployster-gcp-base:${TAG}/g" ./resources/gcp/container/cluster/Dockerfile
+sed -i "s/^FROM .*/FROM gcr.io\/infolinks-gcr\/deployster-gcp-base:${TAG}/g" ./resources/gcp/project/Dockerfile
 
-docker build --tag "gcr.io/infolinks-gcr/deployster-gcp-base:${TAG}" --file "./resources/gcp/base/Dockerfile" ./resources
-docker build --tag "gcr.io/infolinks-gcr/deployster-gcp-compute-address:${TAG}" --file "./resources/gcp/compute/address/Dockerfile" ./resources
-docker build --tag "gcr.io/infolinks-gcr/deployster-gcp-project:${TAG}" --file "./resources/gcp/project/Dockerfile" ./resources
-docker build --tag "gcr.io/infolinks-gcr/deployster-gcp-container-cluster:${TAG}" --file "./resources/gcp/container/cluster/Dockerfile" ./resources
+gcloud docker -- build --tag "gcr.io/infolinks-gcr/deployster-gcp-base:${TAG}" --file "./resources/gcp/base/Dockerfile" ./resources
+gcloud docker -- build --tag "gcr.io/infolinks-gcr/deployster-gcp-compute-address:${TAG}" --file "./resources/gcp/compute/address/Dockerfile" ./resources
+gcloud docker -- build --tag "gcr.io/infolinks-gcr/deployster-gcp-container-cluster:${TAG}" --file "./resources/gcp/container/cluster/Dockerfile" ./resources
+gcloud docker -- build --tag "gcr.io/infolinks-gcr/deployster-gcp-project:${TAG}" --file "./resources/gcp/project/Dockerfile" ./resources
+
+if [[ "${2}" == "push" ]]; then
+    gcloud docker -- push gcr.io/infolinks-gcr/deployster-gcp-base:${TAG}
+    gcloud docker -- push gcr.io/infolinks-gcr/deployster-gcp-compute-address:${TAG}
+    gcloud docker -- push gcr.io/infolinks-gcr/deployster-gcp-container-cluster:${TAG}
+    gcloud docker -- push gcr.io/infolinks-gcr/deployster-gcp-project:${TAG}
+fi
---
+++ .buildkite/release.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+TAG="${1}"
+[[ -z "${TAG}" ]] && echo "usage: $0 <tag> <version>" && exit 1
+
+VERSION="${2}"
+[[ -z "${VERSION}" ]] && echo "usage: $0 <tag> <version>" && exit 1
+
+set -ex
+
+gcloud docker -- tag "gcr.io/infolinks-gcr/deployster-gcp-base:${TAG}" "infolinks/deployster-gcp-base:${VERSION}"
+gcloud docker -- tag "infolinks/deployster-gcp-base:${VERSION}" "infolinks/deployster-gcp-base:latest"
+gcloud docker -- push "infolinks/deployster-gcp-base:${VERSION}"
+gcloud docker -- push "infolinks/deployster-gcp-base:latest"
+
+gcloud docker -- tag "gcr.io/infolinks-gcr/deployster-gcp-compute-address:${TAG}" "infolinks/deployster-gcp-compute-address:${VERSION}"
+gcloud docker -- tag "infolinks/deployster-gcp-compute-address:${VERSION}" "infolinks/deployster-gcp-compute-address:latest"
+gcloud docker -- push "infolinks/deployster-gcp-compute-address:${VERSION}"
+gcloud docker -- push "infolinks/deployster-gcp-compute-address:latest"
+
+gcloud docker -- tag "gcr.io/infolinks-gcr/deployster-gcp-container-cluster:${TAG}" "infolinks/deployster-gcp-container-cluster:${VERSION}"
+gcloud docker -- tag "infolinks/deployster-gcp-container-cluster:${VERSION}" "infolinks/deployster-gcp-container-cluster:latest"
+gcloud docker -- push "infolinks/deployster-gcp-container-cluster:${VERSION}"
+gcloud docker -- push "infolinks/deployster-gcp-container-cluster:latest"
+
+gcloud docker -- tag "gcr.io/infolinks-gcr/deployster-gcp-project:${TAG}" "infolinks/deployster-gcp-project:${VERSION}"
+gcloud docker -- tag "infolinks/deployster-gcp-project:${VERSION}" "infolinks/deployster-gcp-project:latest"
+gcloud docker -- push "infolinks/deployster-gcp-project:${VERSION}"
+gcloud docker -- push "infolinks/deployster-gcp-project:latest"
