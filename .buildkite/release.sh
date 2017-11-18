105d5bd6bf53353e25b9820634e9c4f05d52362c
Arik Kfir <arik@infolinks.com>
2017-11-18 02:49:27 +0200

    CI builds will no longer push images to DockerHub. Release builds will re-build images using the version as the tag.

--- .buildkite/release.sh
+++ .buildkite/release.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-TAG="${1}"
-[[ -z "${TAG}" ]] && echo "usage: $0 <tag> <version>" >&2 && exit 1
-
-VERSION="${2}"
-[[ -z "${VERSION}" ]] && echo "usage: $0 <tag> <version>" >&2 && exit 1
+VERSION="${1}"
+[[ -z "${VERSION}" ]] && echo "usage: $0 <version>" >&2 && exit 1
 
 set -ex
+
+source $(dirname $0)/build.sh "${VERSION}" push
 
 # tag images as VERSION and 'latest', then push them to Docker Hub
 gcloud docker --authorize-only
@@ -15,13 +14,9 @@ for docker_file in $(find "./resources" -name "Dockerfile"); do
     IMAGE_PATH="${IMAGE_DIR//\.\/resources\//}"
     IMAGE_NAME="infolinks/deployster/${IMAGE_PATH//\//-}"
 
-    docker tag "${IMAGE_NAME}:${TAG}" "${IMAGE_NAME}:${VERSION}"
-    docker push "${IMAGE_NAME}:${VERSION}"
-    docker tag "${IMAGE_NAME}:${TAG}" "${IMAGE_NAME}:latest"
+    docker tag "${IMAGE_NAME}:${VERSION}" "${IMAGE_NAME}:latest"
     docker push "${IMAGE_NAME}:latest"
 done
 
-docker tag "infolinks/deployster:${TAG}" "infolinks/deployster:${VERSION}"
-docker push "infolinks/deployster:${VERSION}"
-docker tag "infolinks/deployster:${TAG}" "infolinks/deployster:latest"
+docker tag "infolinks/deployster:${VERSION}" "infolinks/deployster:latest"
 docker push "infolinks/deployster:latest"
