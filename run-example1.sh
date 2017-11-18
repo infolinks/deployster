43ca07f2eb00d598b656598bac310f4563873bb2
Arik Kfir <arik@infolinks.com>
2017-11-18 02:29:28 +0200

    Development script refactor.

--- run-example1.sh
+++ run-example1.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-PROJECT="${1}"
-# TODO: generalize this script
-
 set -ex
 
+PROJECT="${1}"
 ./.buildkite/build.sh local > /dev/null && echo ''
-
-./bin/deployster.sh --var gcp_project=infolinks-test \
-                    --var-file ./examples/context.default.yaml \
-                    ./examples/example1.yaml
+./bin/deployster.sh --var gcp_project=${PROJECT} --var-file ./examples/context.default.yaml ./examples/example1.yaml
