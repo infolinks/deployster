8573d4a45681e05f0e34a5e6668aa2a2260bb7c5
Arik Kfir <arik@kfirs.com>
2017-11-18 03:29:35 +0200

    Docs (#3)
    * Documentation update.
    
    * Removed "bin/deployster.sh" and "run-example1.sh" scripts.
    
    * Add examples paragraph.

--- run-example1.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-PROJECT="${1}"
-./.buildkite/build.sh local > /dev/null && echo ''
-./bin/deployster.sh --var gcp_project=${PROJECT} --var-file ./examples/context.default.yaml ./examples/example1.yaml
