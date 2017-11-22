98944f48c02e88faf75e4fbb2a053b0fca7f36ab
Arik Kfir <arik@infolinks.com>
2017-11-22 16:22:24 +0200

    Update 'release.sh' to find all Docker images according to the new directory structure & naming conventions.

--- .buildkite/release.sh
+++ .buildkite/release.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/env bash
 
-VERSION="${1}"
-[[ -z "${VERSION}" ]] && echo "usage: $0 <version>" >&2 && exit 1
+TAG="${1}"
+[[ -z "${TAG}" ]] && echo "usage: $0 <version>" >&2 && exit 1
 
-set -ex
+echo "Building & pushing versioned images..." >&2
+$(dirname $0)/build.sh "${TAG}" push
 
-echo "Building & pushing versioned images..."
-$(dirname $0)/build.sh "${VERSION}" push
+# setup
+DEPLOYSTER_HOME=$(cd $(dirname $0)/..; pwd)
+RESOURCES_HOME="${DEPLOYSTER_HOME}/resources"
+TAG_PREFIX="infolinks/deployster"
 
-# tag images as VERSION and 'latest', then push them to Docker Hub
+echo "Authenticating to GCR..." >&2
 gcloud docker --authorize-only
-for docker_file in $(find "./resources" -name "Dockerfile"); do
-    IMAGE_DIR="${docker_file//\/Dockerfile/}"
-    IMAGE_PATH="${IMAGE_DIR//\.\/resources\//}"
-    IMAGE_NAME="infolinks/deployster-${IMAGE_PATH//\//-}"
 
-    docker tag "${IMAGE_NAME}:${VERSION}" "${IMAGE_NAME}:latest"
-    docker push "${IMAGE_NAME}:latest"
+# re-tag images with "latest" and push those too
+for dockerfile in $(ls -X ${RESOURCES_HOME}/Dockerfile.*|grep -v ".local"); do
+
+    IMAGE_NAME=$(echo "${dockerfile}" | sed "s/.*\/Dockerfile\.\(.\+\)$/\1/g")
+    IMAGE_NAME=${IMAGE_NAME//_/-}
+
+    echo "Re-tagging Docker image '${TAG_PREFIX}-${IMAGE_NAME}:${TAG}'..." >&2
+    docker tag "${TAG_PREFIX}-${IMAGE_NAME}:${TAG}" "${TAG_PREFIX}-${IMAGE_NAME}:latest"
+    docker push "${TAG_PREFIX}-${IMAGE_NAME}:latest"
 done
 
-docker tag "infolinks/deployster:${VERSION}" "infolinks/deployster:latest"
-docker push "infolinks/deployster:latest"
+# re-tag main deployster image with "latest" and push that too
+echo "Re-tagging main Docker image..." >&2
+docker tag "${TAG_PREFIX}:${TAG}" "${TAG_PREFIX}:latest"
+docker push "${TAG_PREFIX}:latest"
