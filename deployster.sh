5b18b4e57c1e2981e56e4925f93dbba338025d03
Arik Kfir <arik@infolinks.com>
2017-11-22 19:42:04 +0200

    Run the Docker image with "-it" only if running in a terminal.

--- deployster.sh
+++ deployster.sh
@@ -8,8 +8,15 @@ fi
 # default deployster version to "latest" unless supplied as environment variable already
 [[ "${DEPLOYSTER_VERSION}" == "" ]] && DEPLOYSTER_VERSION="latest"
 
+# Docker args
+if [[ -t 1 ]]; then
+    INTERACTIVE_ARGS="-it"
+else
+    INTERACTIVE_ARGS=""
+fi
+
 # run!
-docker run -it \
+docker run ${INTERACTIVE_ARGS} \
            -v $(pwd):/deployster/workspace \
            -v $(pwd)/work:/deployster/work \
            -v /var/run/docker.sock:/var/run/docker.sock \
