f839cac5282713517e8f346d2202c80250943811
Arik Kfir <arik@infolinks.com>
2017-11-28 12:29:47 +0200

    Fix incorrect terminal mode detection in 'deployster.sh'.

--- deployster.sh
+++ deployster.sh
@@ -3,9 +3,12 @@
 # default deployster version to "latest" unless supplied as environment variable already
 [[ "${DEPLOYSTER_VERSION}" == "" ]] && DEPLOYSTER_VERSION="latest"
 
+# set Docker flags
+[[ -t 1 ]] && DOCKER_FLAGS="${DOCKER_FLAGS} -it"
+
 # run!
 mkdir -vp ~/.deployster
-docker run $([[ -t 1 ]] && echo -n "-it" || echo -n "") \
+docker run ${DOCKER_FLAGS} \
            -v $(cd ~/.deployster; pwd):/root/.deployster \
            -v $(pwd):/deployster/workspace \
            -v $(pwd)/work:/deployster/work \
