b4b1e4b52a54fb97e89ca5d0b3232f6e6284c463
Arik Kfir <arik@kfirs.com>
2017-11-29 09:47:20 +0200

    Bug fixes
    - Fix project API enablement/disablement polling.
    - Generate kubectl config file without using gcloud's "get-credentials" command (faster)

--- resources/src/gcp.py
+++ resources/src/gcp.py
@@ -1,7 +1,3 @@
-import argparse
-import subprocess
-from typing import Any, Callable
-
 from dresources import DResource
 
 
@@ -13,10 +9,3 @@ class GcpResource(DResource):
         self.add_plug(name='gcp-service-account',
                       container_path='/deployster/service-account.json',
                       optional=False, writable=False)
-
-    # TODO: remove this code
-    # def execute_action(self, action_name: str, action_method: Callable[..., Any], args: argparse.Namespace):
-    #     if action_name != 'init':
-    #         sa_file_path: str = self.get_plug("gcp-service-account").container_path
-    #         subprocess.run(f"gcloud auth activate-service-account --key-file {sa_file_path}", check=True, shell=True)
-    #     super().execute_action(action_name, action_method, args)
