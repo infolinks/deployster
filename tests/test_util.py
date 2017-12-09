27583ea9306eeefb6bb0c9ba5b682570179fa6d0
Arik Kfir <arik@infolinks.com>
2017-12-10 00:29:59 +0200

    Add Logger header test.

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,7 +1,21 @@
-from util import UserError
+import pytest
+from _pytest.capture import CaptureResult
+
+from util import UserError, Logger
 
 
 def test_new_usererror():
     msg = f"hello!"
     e: UserError = UserError(msg)
     assert e.message == msg
+
+
+@pytest.mark.parametrize("header", ["test header"])
+@pytest.mark.parametrize("indent_amount", [0, 2, 4])
+@pytest.mark.parametrize("spacious", [True, False])
+def test_logger_header(capsys, header: str, indent_amount: int, spacious: bool):
+    with Logger(header=header, indent_amount=indent_amount, spacious=spacious):
+        pass
+
+    captured: CaptureResult = capsys.readouterr()
+    assert captured.out == f'{header}\n' + (f'\n{" " * indent_amount}\n' if spacious else '')
