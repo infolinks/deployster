13f298ef1ebcf905a1e8f165804519fcc815de49
Arik Kfir <arik@infolinks.com>
2017-12-10 01:18:06 +0200

    Add more logger tests

--- tests/test_util.py
+++ tests/test_util.py
@@ -10,12 +10,23 @@ def test_new_usererror():
     assert e.message == msg
 
 
-@pytest.mark.parametrize("header", ["test header"])
+@pytest.mark.parametrize("header", ["test header", None])
 @pytest.mark.parametrize("indent_amount", [0, 2, 4])
 @pytest.mark.parametrize("spacious", [True, False])
-def test_logger_header(capsys, header: str, indent_amount: int, spacious: bool):
-    with Logger(header=header, indent_amount=indent_amount, spacious=spacious):
-        pass
+@pytest.mark.parametrize("content", ["some-line", None])
+def test_logger_header(capsys, header: str, indent_amount: int, spacious: bool, content: str):
+    with Logger(header=header, indent_amount=indent_amount, spacious=spacious) as logger:
+        if content is not None:
+            logger.info(content)
 
     captured: CaptureResult = capsys.readouterr()
-    assert captured.out == f'{header}\n' + (f'\n{" " * indent_amount}\n' if spacious else '')
+    if header is not None:
+        expected = f'{header}\n' + \
+                   (f'\n' if spacious else '') + \
+                   (f'{" " * indent_amount}{content}\n' if content else '') + \
+                   (f'{" " * indent_amount}\n' if spacious else '')
+        assert captured.out == expected
+    elif spacious:
+        expected = (f'{" " * indent_amount}{content}\n' if content else '') + \
+                   (f'{" " * indent_amount}\n' if spacious else '')
+        assert captured.out == expected
