337ddecd9d447891ac9ecdacedb207a6b98a3224
Arik Kfir <arik@infolinks.com>
2017-12-10 23:58:17 +0200

    Changes in Cloud SQL resource week-day translation tests

--- tests/test_resource_gcp_cloud_sql.py
+++ tests/test_resource_gcp_cloud_sql.py
@@ -2,8 +2,26 @@ import json
 
 import pytest
 
-from gcp_cloud_sql import GcpCloudSql
+from gcp_cloud_sql import GcpCloudSql, _translate_day_name_to_number
 from mock_gcp_services import MockGcpServices, load_scenarios
+
+
+@pytest.mark.parametrize("day_name,expected", [
+    ("Monday", 1),
+    ("Tuesday", 2),
+    ("Wednesday", 3),
+    ("Thursday", 4),
+    ("Friday", 5),
+    ("Saturday", 6),
+    ("Sunday", 7),
+    ("Unknown", None),
+])
+def test_week_day_translation(day_name: str, expected: int):
+    if expected is None:
+        with pytest.raises(Exception, match='unknown week-day encountered'):
+            _translate_day_name_to_number(day_name)
+    else:
+        assert _translate_day_name_to_number(day_name) == expected
 
 
 @pytest.mark.parametrize("actual,config,expected",
