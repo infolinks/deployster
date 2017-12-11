118173902fb8aee7aa92fb4752b76ca02058603e
Arik Kfir <arik@infolinks.com>
2017-12-11 22:06:36 +0200

    Pass extra arguments in "test.sh".
    This enables invoking it like so:
    
        test.sh -vv

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -7,4 +7,6 @@ docker run --env COVERALLS_REPO_TOKEN="${COVERALLS_REPO_TOKEN}" \
            --env BUILDKITE="${BUILDKITE}" \
            --env BUILDKITE_JOB_ID="${BUILDKITE_JOB_ID}" \
            --env BUILDKITE_BRANCH="${BUILDKITE_BRANCH}" \
-           --tty infolinks/deployster:test-${BUILDKITE_COMMIT}
+           --tty \
+           infolinks/deployster:test-${BUILDKITE_COMMIT} \
+           $@
