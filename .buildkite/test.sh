e71f8ddba418c658bf818de97e77c92da83123d6
Arik Kfir <arik@infolinks.com>
2017-12-15 11:50:33 +0200

    Build testing Docker image in the standard (more-verbose) mode.
    This shows progress of the build better.

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker build -q --file ./Dockerfile.test --tag infolinks/deployster:test-${BUILDKITE_COMMIT} .
+docker build --file ./Dockerfile.test --tag infolinks/deployster:test-${BUILDKITE_COMMIT} .
 docker run --env COVERALLS_REPO_TOKEN="${COVERALLS_REPO_TOKEN}" \
            --env BUILDKITE="${BUILDKITE}" \
            --env BUILDKITE_JOB_ID="${BUILDKITE_JOB_ID}" \
