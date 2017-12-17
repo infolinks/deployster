18df2500c2f8efac39e48b935346a5f7a56486c4
Arik Kfir <arik@infolinks.com>
2017-12-17 16:08:48 +0200

    Kubernetes "Secret" resource should decode its data upon fetching its state from Kubernetes.
    Also added some tests to the Kubernetes "Secret" resource.

--- resources/src/k8s_secret.py
+++ resources/src/k8s_secret.py
@@ -1,4 +1,6 @@
+from base64 import b64decode
 from base64 import b64encode
+from copy import deepcopy
 
 from external_services import ExternalServices
 from k8s import K8sResource
@@ -8,6 +10,13 @@ class K8sSecret(K8sResource):
 
     def __init__(self, data: dict, svc: ExternalServices = ExternalServices()) -> None:
         super().__init__(data=data, svc=svc)
+
+    def discover_state(self):
+        manifest = super().discover_state()
+        if manifest is not None and 'data' in manifest and manifest['data'] is not None:
+            manifest = deepcopy(manifest)
+            manifest['data'] = {key: b64decode(str(val).encode()).decode() for key, val in manifest['data'].items()}
+        return manifest
 
     def build_kubectl_manifest(self) -> dict:
         manifest: dict = super().build_kubectl_manifest()
