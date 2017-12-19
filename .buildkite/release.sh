d0dd13f7a06b36684e1c9df76ae5fc0ea64fd6cf
Arik Kfir <arik@infolinks.com>
2017-12-19 18:15:17 +0200

    Release should add "latest" tag to Kubernetes resources that do not have Dockerfile (aliases of "k8s" image)

--- .buildkite/release.sh
+++ .buildkite/release.sh
@@ -25,6 +25,33 @@ for dockerfile in $(ls -X ${RESOURCES_HOME}/Dockerfile.*|grep -v ".local"); do
     docker push "${TAG_PREFIX}-${IMAGE_NAME}:latest"
 done
 
+# re-tag k8s synonyms
+docker tag "${TAG_PREFIX}-${IMAGE_NAME}:${TAG}" "${TAG_PREFIX}-${IMAGE_NAME}:latest"
+docker tag "infolinks/deployster-k8s-clusterrole:${TAG}" "infolinks/deployster-k8s-clusterrole:latest"
+docker tag "infolinks/deployster-k8s-clusterrolebinding:${TAG}" "infolinks/deployster-k8s-clusterrolebinding:latest"
+docker tag "infolinks/deployster-k8s-configmap:${TAG}" "infolinks/deployster-k8s-configmap:latest"
+docker tag "infolinks/deployster-k8s-cronjob:${TAG}" "infolinks/deployster-k8s-cronjob:latest"
+docker tag "infolinks/deployster-k8s-daemonset:${TAG}" "infolinks/deployster-k8s-daemonset:latest"
+docker tag "infolinks/deployster-k8s-deployment:${TAG}" "infolinks/deployster-k8s-deployment:latest"
+docker tag "infolinks/deployster-k8s-horizontalpodautoscaler:${TAG}" "infolinks/deployster-k8s-horizontalpodautoscaler:latest"
+docker tag "infolinks/deployster-k8s-ingress:${TAG}" "infolinks/deployster-k8s-ingress:latest"
+docker tag "infolinks/deployster-k8s-job:${TAG}" "infolinks/deployster-k8s-job:latest"
+docker tag "infolinks/deployster-k8s-namespace:${TAG}" "infolinks/deployster-k8s-namespace:latest"
+docker tag "infolinks/deployster-k8s-networkpolicy:${TAG}" "infolinks/deployster-k8s-networkpolicy:latest"
+docker tag "infolinks/deployster-k8s-node:${TAG}" "infolinks/deployster-k8s-node:latest"
+docker tag "infolinks/deployster-k8s-persistentvolume:${TAG}" "infolinks/deployster-k8s-persistentvolume:latest"
+docker tag "infolinks/deployster-k8s-persistentvolumeclaim:${TAG}" "infolinks/deployster-k8s-persistentvolumeclaim:latest"
+docker tag "infolinks/deployster-k8s-pod:${TAG}" "infolinks/deployster-k8s-pod:latest"
+docker tag "infolinks/deployster-k8s-replicaset:${TAG}" "infolinks/deployster-k8s-replicaset:latest"
+docker tag "infolinks/deployster-k8s-replicationcontroller:${TAG}" "infolinks/deployster-k8s-replicationcontroller:latest"
+docker tag "infolinks/deployster-k8s-role:${TAG}" "infolinks/deployster-k8s-role:latest"
+docker tag "infolinks/deployster-k8s-rolebinding:${TAG}" "infolinks/deployster-k8s-rolebinding:latest"
+docker tag "infolinks/deployster-k8s-secret:${TAG}" "infolinks/deployster-k8s-secret:latest"
+docker tag "infolinks/deployster-k8s-service:${TAG}" "infolinks/deployster-k8s-service:latest"
+docker tag "infolinks/deployster-k8s-serviceaccount:${TAG}" "infolinks/deployster-k8s-serviceaccount:latest"
+docker tag "infolinks/deployster-k8s-statefulset:${TAG}" "infolinks/deployster-k8s-statefulset:latest"
+docker tag "infolinks/deployster-k8s-storageclass:${TAG}" "infolinks/deployster-k8s-storageclass:latest"
+
 # re-tag main deployster image with "latest" and push that too
 echo "Re-tagging main Docker image..." >&2
 docker tag "${TAG_PREFIX}:${TAG}" "${TAG_PREFIX}:latest"
